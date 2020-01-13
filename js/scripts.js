1878ca88e6f2f958065797a7d67a5240d6b341a4
Student <example@example.com>
2020-01-13 14:37:44 -0800

    make a calculator

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+};
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number"));
+alert(add(number1, number2));
+
+//subtract
+var minus = function(number1, number2) {
+    return number1 - number2;
+}
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter anoter number"));
+alert(minus(number1,number2));
+
+//multiplication
+var minus = function(number1, number2) {
+    return number1 * number2;
+}
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter anoter number"));
+alert(minus(number1,number2));
+
+//division
+var minus = function(number1, number2) {
+    return number1 / number2;
+}
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter anoter number"));
+alert(minus(number1,number2));
+
+
